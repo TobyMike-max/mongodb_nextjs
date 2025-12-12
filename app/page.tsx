1174b206d37ee0be619a1cb8c36d0efd375d57de
TobyMike-max <osinubi.michael@gmail.com>
2025-12-12 06:33:34 +0100

    Implement user model, database connection, and user creation API

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import axios from "axios";
 import { useState } from "react";
 
@@ -7,19 +9,24 @@ export default function Home() {
     email: string;
   }
 
-  const [name, setName] = useState<string>("")
-  const [email, setEmail] = useState<string>("")
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
 
-  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    e.preventDefault()
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
     try {
-      const response = await axios.post<FormData>('/api/users', {name, email})
-    } catch(err) {
-      console.log(err)
+      const response = await axios.post<FormData>("/api/users", {
+        name,
+        email,
+      });
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
   return (
-    <div className="max-w-lg mx-auto">
+    <div className="max-w-lg mx-auto mt-5">
       <h1 className="text-2xl font-bold mb-4">Create User</h1>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
@@ -29,14 +36,18 @@ export default function Home() {
             placeholder="Enter Name"
             className="mt-1 block px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             required
-            onChange={(e) => {setName(e.target.value)}}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           ></input>
         </div>
         <div>
           <label className="block text-sm font-medium">Email</label>
           <input
             type="email"
-            onChange={(e) => {setEmail(e.target.value)}}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             placeholder="Enter Email"
             className="mt-1 block px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             required
